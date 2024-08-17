773b897443129888b69fa149a1820ab0224d4c6e
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-08-17 14:43:14 +0530

    Create LongestIncreasingSubsequenceUsingBinarySearch.java

---
+++ LongestIncreasingSubsequenceUsingBinarySearch.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Function to find length of longest increasing subsequence.
+    static int longestSubsequence(int n, int arr[]) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(arr[0]);
+
+        int len = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > temp.get(temp.size() - 1)) {
+                temp.add(arr[i]);
+                len++;
+            } else {
+                int ind = Collections.binarySearch(temp, arr[i]);
+                if (ind < 0) {
+                    ind = -ind - 1;
+                }
+                temp.set(ind, arr[i]);
+            }
+        }
+
+        return len;
+    }
+}
