db4722f7bd52c9fe76355101e48f80ce038bf7dc
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-08-18 19:41:46 +0530

    Create MaximalRectangle.java

---
+++ MaximalRectangle.java
@@ -0,0 +1,53 @@
+class Solution {
+    public static int largestRectangleArea(int[] height) {
+    if (height == null || height.length == 0) {
+        return 0;
+    }
+    int[] lessFromLeft = new int[height.length]; // idx of the first bar the left that is lower than current
+    int[] lessFromRight = new int[height.length]; // idx of the first bar the right that is lower than current
+    lessFromRight[height.length - 1] = height.length;
+    lessFromLeft[0] = -1;
+
+    for (int i = 1; i < height.length; i++) {
+        int p = i - 1;
+
+        while (p >= 0 && height[p] >= height[i]) {
+            p = lessFromLeft[p];
+        }
+        lessFromLeft[i] = p;
+    }
+
+    for (int i = height.length - 2; i >= 0; i--) {
+        int p = i + 1;
+
+        while (p < height.length && height[p] >= height[i]) {
+            p = lessFromRight[p];
+        }
+        lessFromRight[i] = p;
+    }
+
+    int maxArea = 0;
+    for (int i = 0; i < height.length; i++) {
+        maxArea = Math.max(maxArea, height[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+    }
+
+    return maxArea;
+    }
+    public int maximalRectangle(char[][] matrix) {
+        int maxArea=0;
+        int height[]=new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]=='1'){
+                    height[j]++;
+                }
+                else{
+                    height[j]=0;
+                }
+            }
+            int area = largestRectangleArea(height);
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+}
